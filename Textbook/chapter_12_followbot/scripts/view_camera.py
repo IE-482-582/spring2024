fe04ac2430ca1617be4a1516795119cf63f644bd
Chase Murray <cmurray3@buffalo.edu>
2024-03-05 10:55:34 -0500

    followbot. with bugs

---
+++ Textbook/chapter_12_followbot/scripts/view_camera.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# NOTE:  The textbook calls this script "follower_opencv.py".
+# We've renamed it because this script doesn't actually move the robot.
+
+import rospy
+from sensor_msgs.msg import Image
+import cv2, cv_bridge
+
+class ViewCamera:
+	def __init__(self):
+		self.bridge = cv_bridge.CvBridge()
+		# cv2.namedWindow("window", 1)
+		self.image_sub = rospy.Subscriber(camTopic, 
+									  Image, self.image_callback)
+	def image_callback(self, msg):
+		# print(msg.height, msg.width)
+		# print(msg.data)
+		image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
+		cv2.imshow("window", image)
+		cv2.waitKey(3)
+
+rospy.init_node('view_camera', anonymous=True)
+
+camTopic = rospy.get_param("~camTopic", "/realsense/color/image_raw")
+follower = ViewCamera()
+rospy.spin()
+
